20e03e53db049453593725d9740a833804a57f03
Gabriel Munakata <gabrielyuto2016@gmail.com>
2025-01-07 22:09:15 -0300

    Novas implementacoes

---
+++ app/models/book.rb
@@ -0,0 +1,10 @@
+class Book < ApplicationRecord
+  belongs_to :bookcase
+  belongs_to :author
+  validate :is_the_bookcase_on_limit
+
+  def is_the_bookcase_on_limit
+    if (self.bookcase.books.count >= self.bookcase.limit)
+      errors.add(:base, "A estante já está preenchida, não cabe mais livros!")
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :books
+  resources :authors
+  resources :bookcases
   get "up" => "rails/health#show", as: :rails_health_check
   get '/hello', to: 'teste#hello'
 end
